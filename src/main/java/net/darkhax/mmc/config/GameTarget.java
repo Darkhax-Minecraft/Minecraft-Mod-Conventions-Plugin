c9e7b7c2b10833c5ad58b476ac6a05f0a0d493b5
Tyler Hancock <darklime@live.ca>
2026-04-02 14:02:06 -0600

    Change game target to Minecraft 26.1.1.

--- src/main/java/net/darkhax/mmc/config/GameTarget.java
+++ src/main/java/net/darkhax/mmc/config/GameTarget.java
@@ -4,7 +4,7 @@ import net.darkhax.mmc.Util;
 
 public record GameTarget(int javaVersion, String gameVersion, String neoform, String neoforge, String fabric, String fabricLoader) {
 
-    public static final GameTarget TARGET = new GameTarget(25, "26.1", "26.1-1", "26.1.0.1-beta", "0.144.0+26.1", "0.18.4");
+    public static final GameTarget TARGET = new GameTarget(25, "26.1.1", "26.1.1-1", "26.1.1.1-beta", "0.145.3+26.1.1", "0.18.6");
 
     public String adjustedGameVersion() {
         return Util.countChar(this.gameVersion, '.') == 1 ? this.gameVersion + ".0" : this.gameVersion;
