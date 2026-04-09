b80d3fad20940f549ecbb45a0590506bbc2903cc
Tyler Hancock <darklime@live.ca>
2026-04-09 14:01:51 -0600

    Update Fabric API and NeoForge API versions.

--- src/main/java/net/darkhax/mmc/config/GameTarget.java
+++ src/main/java/net/darkhax/mmc/config/GameTarget.java
@@ -6,7 +6,7 @@ import java.util.Set;
 
 public record GameTarget(int javaVersion, String gameVersion, String neoform, String neoforge, String fabric, String fabricLoader, Set<String> compatibleWith) {
 
-    public static final GameTarget TARGET = new GameTarget(25, "26.1.1", "26.1.1-1", "26.1.1.1-beta", "0.145.3+26.1.1", "0.18.6", Set.of("26.1"));
+    public static final GameTarget TARGET = new GameTarget(25, "26.1.1", "26.1.1-1", "26.1.1.15-beta", "0.145.4+26.1.1", "0.18.6", Set.of("26.1"));
 
     public String adjustedGameVersion() {
         return Util.countChar(this.gameVersion, '.') == 1 ? this.gameVersion + ".0" : this.gameVersion;
