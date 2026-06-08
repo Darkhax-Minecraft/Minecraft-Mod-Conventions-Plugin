bd39e293a0f47cda230b5ae61b759f6167270e97
Tyler Hancock <darklime@live.ca>
2026-06-07 21:30:00 -0600

    Update NeoForge and Fabric versions.

--- src/main/java/net/darkhax/mmc/config/GameTarget.java
+++ src/main/java/net/darkhax/mmc/config/GameTarget.java
@@ -6,7 +6,7 @@ import java.util.Set;
 
 public record GameTarget(int javaVersion, String gameVersion, String minGameVersion, String neoform, String neoforge, String minNeoforge, String fabric, String minFabric, String fabricLoader, String minFabricLoader, Set<String> compatibleWith) {
 
-    public static final GameTarget BUILTIN = new GameTarget(25, "26.1.2", "26.1", "26.1.2-1", "26.1.2.7-beta", "26.1.0.1-beta", "0.145.4+26.1.2", "0.145.1+26.1", "0.19.1", "0.18.6", Set.of("26.1", "26.1.1"));
+    public static final GameTarget BUILTIN = new GameTarget(25, "26.1.2", "26.1", "26.1.2-1", "26.1.2.74", "26.1.0.1-beta", "0.151.0+26.1.2", "0.145.1+26.1", "0.19.3", "0.18.6", Set.of("26.1", "26.1.1"));
 
     public String adjustedGameVersion() {
         return Util.countChar(this.gameVersion, '.') == 1 ? this.gameVersion + ".0" : this.gameVersion;
