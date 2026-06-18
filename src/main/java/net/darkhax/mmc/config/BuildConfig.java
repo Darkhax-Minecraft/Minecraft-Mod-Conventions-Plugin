6b8aa069ba2410fbb91528e673e4d72d3e5fe606
Tyler Hancock <darklime@live.ca>
2026-06-17 18:48:18 -0600

    Setup a local runtime configuration for dependencies.

--- src/main/java/net/darkhax/mmc/config/BuildConfig.java
+++ src/main/java/net/darkhax/mmc/config/BuildConfig.java
@@ -2,6 +2,7 @@ package net.darkhax.mmc.config;
 
 import net.darkhax.mmc.ConventionsPlugin;
 import org.gradle.api.Project;
+import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.artifacts.dsl.DependencyHandler;
 
 import javax.annotation.Nullable;
@@ -11,13 +12,19 @@ import java.util.Locale;
 public record BuildConfig(ModData mod, HostedProject curseforge, HostedProject modrinth, @Nullable List<Dependency> dependencies, Boolean enable_compat_mode) {
 
     public void applyDependencies(Project project, Platform platform) {
+        // To avoid adding local runtime dependencies to maven and module meta,
+        // we create a local runtime and use that instead.
+        final Configuration runtime = project.getConfigurations().getByName("runtimeClasspath");
+        final Configuration localRuntime = project.getConfigurations().maybeCreate("localRuntime");
+        runtime.extendsFrom(localRuntime);
         final DependencyHandler projectDeps = project.getDependencies();
         if (this.dependencies != null) {
             for (Dependency depInfo : this.dependencies) {
                 final String depNotation = depInfo.maven().get(platform);
                 if (depNotation != null) {
                     ConventionsPlugin.LOGGER.lifecycle("Adding {} dependency '{}' to {}.", depInfo.type().name().toLowerCase(Locale.ROOT), depNotation, project.getDisplayName());
-                    projectDeps.add("implementation", depNotation);
+                    projectDeps.add("localRuntime", depNotation);
+                    projectDeps.add("compileOnly", depNotation);
                 }
             }
         }
