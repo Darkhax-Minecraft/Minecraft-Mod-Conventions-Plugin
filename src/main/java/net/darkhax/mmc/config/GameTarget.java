aa40b9848b4c9b6dcf2a4e0dbb4c0f9a001861d9
Tyler Hancock <darklime@live.ca>
2026-07-07 23:06:52 -0600

    Update loader dependencies.

--- src/main/java/net/darkhax/mmc/config/GameTarget.java
+++ src/main/java/net/darkhax/mmc/config/GameTarget.java
@@ -6,7 +6,7 @@ import java.util.Set;
 
 public record GameTarget(int javaVersion, String gameVersion, String minGameVersion, String neoform, String neoforge, String minNeoforge, String fabric, String minFabric, String fabricLoader, String minFabricLoader, Set<String> compatibleWith) {
 
-    public static final GameTarget BUILTIN = new GameTarget(25, "26.2", "26.2", "26.2-1", "26.2.0.1-beta", "26.2.0.1-beta", "0.152.1+26.2", "0.152.1+26.2", "0.19.3", "0.19.3", Set.of());
+    public static final GameTarget BUILTIN = new GameTarget(25, "26.2", "26.2", "26.2-1", "26.2.0.8-beta", "26.2.0.8-beta", "0.154.2+26.2", "0.154.2+26.2", "0.19.3", "0.19.3", Set.of());
 
     public String adjustedGameVersion() {
         return Util.countChar(this.gameVersion, '.') == 1 ? this.gameVersion + ".0" : this.gameVersion;
